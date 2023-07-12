f5204287abbc493771167e5676c39e2458901bd7
RMBI <rmbi_github@163.com>
2023-07-12 21:33:40 +0800

    Parent_Project

---
+++ XC-ONLINE-BASE/src/main/java/com/xconline/XcOnlineBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xconline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcOnlineBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XcOnlineBaseApplication.class, args);
+    }
+
+}
---
+++ XC-ONLINE-BASE/src/test/java/com/xconline/XcOnlineBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xconline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcOnlineBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
